6cd11d8865aba0be8c11aa770f8306d721c93ea9
Sumatha <sumatha.panthagani@capgemini.com>
2016-11-14 14:20:16 +0530

    custom folder added

---
+++ hybris/bin/custom/epsalesb2bcommerceservices/gensrc/com/epsalesb2bcommerceservices/constants/GeneratedEpsalesb2bcommerceservicesConstants.java
@@ -0,0 +1,32 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Nov 11, 2016 12:29:59 PM                    ---
+ * ----------------------------------------------------------------
+ */
+package com.epsalesb2bcommerceservices.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedEpsalesb2bcommerceservicesConstants
+{
+	public static final String EXTENSIONNAME = "epsalesb2bcommerceservices";
+	public static class TC
+	{
+		public static final String PRODUCTQUANTITYTHRESHOLDFREEGIFTPROMOTION = "ProductQuantityThresholdFreeGiftPromotion".intern();
+	}
+	public static class Attributes
+	{
+		// no constants defined.
+	}
+	
+	protected GeneratedEpsalesb2bcommerceservicesConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ hybris/bin/custom/epsalesb2bcommerceservices/src/com/epsalesb2bcommerceservices/constants/Epsalesb2bcommerceservicesConstants.java
@@ -0,0 +1,29 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2016 hybris AG
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ *
+ *  
+ */
+package com.epsalesb2bcommerceservices.constants;
+
+/**
+ * Global class for all Epsalesb2bcommerceservices constants. You can add global constants for your extension into this class.
+ */
+public final class Epsalesb2bcommerceservicesConstants extends GeneratedEpsalesb2bcommerceservicesConstants
+{
+	public static final String EXTENSIONNAME = "epsalesb2bcommerceservices";
+
+	private Epsalesb2bcommerceservicesConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+}
